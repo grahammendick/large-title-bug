a3e97accb7720d445469021d83211ee29bfc171c
Graham Mendick <graham.mendick@gmail.com>
2022-05-29 21:28:36 +0100

    Added explanation text

--- newarch/Hello.js
+++ newarch/Hello.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Button, SafeAreaView} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
 import {NavigationContext} from 'navigation-react';
 import {NavigationBar} from 'navigation-react-native';
 
@@ -14,6 +14,12 @@ const Hello = () => {
           stateNavigator.navigate('world');
         }}
       />
+      <Text>
+        With the new React Native architecture, large titles only work the first
+        time you see them. Press the button and you'll see a large 'World' title
+        on the next screen. If you go back and press the button again then the
+        'World' title is small instead.
+      </Text>
     </SafeAreaView>
   );
 };
